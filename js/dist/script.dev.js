16a2c33658ea07772b80c98102f2a8451b9a9889
Ahmed111222333 <68826015+Ahmed111222333@users.noreply.github.com>
2020-11-20 18:11:16 +0200

    1

---
+++ js/dist/script.dev.js
@@ -0,0 +1,29 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".client-carousel").owlCarousel({
+    item: 1,
+    rtl: true,
+    dots: false,
+    responsiveClass: true,
+    center: true,
+    margin: 20,
+    responsive: {
+      0: {
+        items: 2,
+        nav: true,
+        loop: true
+      },
+      600: {
+        items: 3,
+        nav: false,
+        loop: true
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: true
+      }
+    }
+  });
+});
